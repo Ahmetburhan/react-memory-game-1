5fc2ffbe94f85aa58b3b2058e41fe87c341b36c9
Anna Zhu <zhuanna93@gmail.com>
2016-04-01 20:29:59 -0700

    Added babel pluggins to compile ES2015 spread to ES5

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,8 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          presets: ['react', 'es2015' ]
+          presets: ['react', 'es2015' ],
+          plugins: ["transform-es2015-spread", "transform-object-rest-spread"]
         }
       }
     ]
